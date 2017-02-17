bbcf696d5513a2bc194481b60e22eb66fc64b475
Jess Green <ardath.ksheyna78@gmail.com>
2017-02-16 20:40:13 -0600

    Adding unit tests and dependencies

---
+++ src/test/java/com/jesgs/moonfx/MoonFxTest.java
@@ -0,0 +1,22 @@
+package com.jesgs.moonfx;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by jess on 2/16/17.
+ */
+public class MoonFxTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+}
